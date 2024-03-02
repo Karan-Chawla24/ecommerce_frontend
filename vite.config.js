e098cbb8784760c5c77267559f842c5567aea8a5
KaranChawla <chawlakaran663@gmail.com>
2024-03-02 15:02:31 +0530

    update vite config file

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    mode: "production",
+    target: "modules",
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+      },
+    },
   },
   plugins: [react()],
   server: {
